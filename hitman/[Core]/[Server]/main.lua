d38cd47d04d1259399110910ddc6987638275de3
broimsuperman <ellisond550@gmail.com>
2024-03-25 19:20:17 -0400

    Add devmode and pingp commands

--- hitman/[Core]/[Server]/main.lua
+++ hitman/[Core]/[Server]/main.lua
@@ -70,8 +70,32 @@ print("https://discord.gg/3ZU6zj3")
 
 
 
+local devmode = true
+-- if devmode is true, the script will enable the ExecuteCommand("refresh") command by typing /devrefresh in the chat.
+RegisterCommand("devrefresh", function()
+  if devmode then
+    ExecuteCommand("refresh")
+  end
+end, false)
 
-
+RegisterCommand("pingp", function(source, args, rawCommand)
+  -- If the source is > 0, then that means it must be a player.
+  if (source > 0) then
+  
+      -- result (using the default GTA:O chat theme) https://i.imgur.com/TaCnG09.png
+      TriggerClientEvent("chat:addMessage", -1, {
+          args = {
+              GetPlayerName(source),
+              "PONG!"
+          },
+          color = { 5, 255, 255 }
+      })
+  
+  -- If it's not a player, then it must be RCON, a resource, or the server console directly.
+  else
+      print("This command was executed by the server console, RCON client, or a resource.")
+  end
+end, false --[[this command is not restricted, everyone can use this.]])
 
 
 
