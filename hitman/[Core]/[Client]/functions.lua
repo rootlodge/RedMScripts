0c2d7294da07aa921ce42be64a32b318170c23f9
broimsuperman <ellisond550@gmail.com>
2024-03-26 02:28:53 -0400

    Add Wait function and VORPcore import

--- hitman/[Core]/[Client]/functions.lua
+++ hitman/[Core]/[Client]/functions.lua
@@ -1,6 +1,9 @@
 --------------------------------------------------------------------------------
 ----------------------------------- RootLodge -----------------------------------
 --------------------------------------------------------------------------------
+local VORPcore = exports.vorp_core:GetCore() -- NEW includes  new callback system
+function Wait(args) Citizen.Wait(args) end
+
 function DrawCircle(x, y, z, r, g, b, a)
   Citizen.InvokeNative(0x2A32FAA57B937173, 0x94FDAE17, x, y, z - 0.95, 0, 0, 0, 0, 0, 0, 1.0, 1.0, 0.25, r, g, b, a, 0, 0, 2, 0, 0, 0, 0)
 end
