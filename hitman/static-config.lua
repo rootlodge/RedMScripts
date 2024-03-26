7a78c51050107861e61b6f7a1a17f6f0810ffce8
broimsuperman <ellisond550@gmail.com>
2024-03-26 04:00:46 -0400

    Update payment and XP configurations in static-config.lua, remove warmenu entirely

--- hitman/static-config.lua
+++ hitman/static-config.lua
@@ -1,30 +1,18 @@
 -- THIS WILL NOT UPDATE VIA THE AUTO UPDATER
 -- THIS CONTAINS THE CONFIGURATION FOR THINGS YOU DON'T WANT OVERWRITTEN
 
--- Here you can choose how you want your UI to look like.
--- NOTE: That you can only have 1 enabled at the same time.
--- If 1 is set to [ true ] the other one needs to be [ false ]
+Config.Payment = {
+  Money = {
+    BPK = true,        -- true = Pay per kill  |  false = fixed amount per kill
+    Min = 5,           -- Minimal money value a player can get per kill.
+    Max = 7,           -- Maximum money value a player can get per kill
+    Static = 10        -- Fixed value of money per bounty kill
+  },
 --------------------------------------------------------------------------------
--- Same thing counts for the Themes. Only 1 can be activated at once!
---------------------------------------------------------------------------------
-Config.NUI = {
-    Theme = { Dark = false, Red = true },
-    Position = { Right = true, Left = false, Center = false }
+  XP = {
+    XPK = true,       -- true = XP per kill  |  false = fixed amount per kill
+    Min = 25,         -- Minimal XP value a player can get per kill.
+    Max = 75,         -- Maximum XP value a player can get per kill
+    Static = 50        -- Fixed value of XP per bounty kill
   }
-  
-  --------------------------------------------------------------------------------
-  Config.Payment = {
-    Money = {
-      BPK = true,        -- true = Pay per kill  |  false = fixed amount per kill
-      Min = 5,           -- Minimal money value a player can get per kill.
-      Max = 7,           -- Maximum money value a player can get per kill
-      Static = 10        -- Fixed value of money per bounty kill
-    },
-  --------------------------------------------------------------------------------
-    XP = {
-      XPK = true,       -- true = XP per kill  |  false = fixed amount per kill
-      Min = 25,         -- Minimal XP value a player can get per kill.
-      Max = 75,         -- Maximum XP value a player can get per kill
-      Static = 50        -- Fixed value of XP per bounty kill
-    }
-  }
+}
