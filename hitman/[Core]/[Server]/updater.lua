a11cb9e8d71a0913e343eb6c0a661c2aacc0ea57
broimsuperman <ellisond550@gmail.com>
2024-03-26 15:49:00 -0400

    Add Config.CheckForUpdates flag and remove versions.json file

---
+++ hitman/[Core]/[Server]/updater.lua
@@ -0,0 +1,25 @@
+if Config.CheckForUpdates then
+    local function VersionLog(_type, log)
+        local color = _type == 'success' and '^2' or '^1'
+        print(('^8[Hitman Contracts]%s %s^7'):format(color, log))
+    end
+
+    local function CheckMenuVersion()
+        PerformHttpRequest('https://.txt', function(err, text, headers)
+            local currentVersion = GetResourceMetadata(GetCurrentResourceName(), 'version')
+            if not text then
+                VersionLog('error', 'Currently unable to run a version check.')
+                return
+            end
+            VersionLog('success', ('Current Version: %s'):format(currentVersion))
+            VersionLog('success', ('Latest Version: %s'):format(text))
+            if text:gsub("%s+", "") == currentVersion:gsub("%s+", "") then
+                VersionLog('success', 'You are running the latest version of Hitman Contracts!')
+            else
+                VersionLog('error', ('You are currently running an outdated version of Hitman Contracts, please update to version %s'):format(text))
+            end
+        end)
+    end
+
+    CheckMenuVersion()
+end
