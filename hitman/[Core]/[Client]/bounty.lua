5054aee3c358ab984e9ee87d228a8fcad9654a52
broimsuperman <ellisond550@gmail.com>
2024-03-29 01:29:17 -0400

    Fix blip removal and entity deletion

--- hitman/[Core]/[Client]/bounty.lua
+++ hitman/[Core]/[Client]/bounty.lua
@@ -38,7 +38,7 @@ local SearchingBodies = false
 local GPSToBodyIsSet = false
 local SaveGuard = false
 local GPStoSDboardactive = false
-
+hashedenemyblipglobal = nil
 --local unashedblip = 'blip_mp_attack_target'
 --local hashedblip = GetHashKey(unashedblip)
 --BlipAddForEntity(hashedblip, rawpeds)
@@ -69,6 +69,7 @@ AddEventHandler('RootLodge:HitContracts:C:SetUpMission', function()
         Citizen.InvokeNative(0x283978A15512B2FE, CreateNPC[k], true)
         --Citizen.InvokeNative(0x23f74c2fda6e7c61, 639638961, CreateNPC[k])
         --addBlipForCoords("GROUP OF TARGETS", 1366733613, v.Coords.x, v.Coords.y, v.Coords.z)
+        let hashedenemyblipglobal = GetHashKey("blip_ambient_marked_for_death")
         addBlipForCoords("Contract Target",GetHashKey("blip_ambient_marked_for_death"),{v.Coords.x,v.Coords.y,v.Coords.z})
         NPCx, NPCy, NPCz = v.x, v.y, v.z
         GiveWeaponToPed_2(CreateNPC[k], rWeapon, 50, true, true, 1, false, 0.5, 1.0, 1.0, true, 0, 0)
@@ -108,6 +109,7 @@ AddEventHandler('RootLodge:HitContracts:C:SetUpMission', function()
               SetGpsMultiRouteRender(false)
               CenterBottomNotify('You managed to kill all targets', 5000)
               SearchingBodies = true
+              RemoveBlip(hashedenemyblipglobal)
               Wait(5000)
               CenterBottomNotify('Search the body for evidence to confirm the kill!', 5000)
               while SearchingBodies do Wait(1)
@@ -145,8 +147,8 @@ AddEventHandler('RootLodge:HitContracts:C:SetUpMission', function()
     MissionStatus = false
     ClearGpsMultiRoute()
     SetGpsMultiRouteRender(false)
-    for k, v in pairs(ArrayTargets[k]) do DeletePed(v) Wait(500) end
-    for k, v in pairs(CreateNPC[k]) do DeletePed(v) Wait(500) end
+    for k, v in pairs(ArrayTargets[k]) do DeleteEntity(v) Wait(500) end
+    for k, v in pairs(CreateNPC[k]) do DeleteEntity(v) Wait(500) end
     ArrayTargets = {}
     CreateNPC = {}
     TotalEnemies = 0
